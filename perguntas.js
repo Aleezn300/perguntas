45065e94c09f89b9ce71423591566d669cc06c4e
Aleezn300 <143625625+Aleezn300@users.noreply.github.com>
2024-09-25 09:07:16 -0300

    g

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Ciencia',
+    'Ciencia'
     'Qual é o gás mais abundante na atmosfera da Terra?'
     'O nitrogênio, que compõe cerca de 78% da atmosfera'
 )
 
 criaCartao(
-    'Geografia',
+    'Geografia'
     'Qual é o maior continente do mundo?'
     'A Ásia é o maior continente do mundo, tanto em área quanto em população'
 )
 
 criaCartao(
     'Geografia',
-    'Qual é a capital do Brasil?',
+    'Qual é a capital do Brasil?'
     'Brasília.'
 )
 
 criaCartao(
-    'Conhecimentos gerais',
-    'Qual é o esporte mais popular do mundo?',
+    'Conhecimentos gerais'
+    'Qual é o esporte mais popular do mundo?'
     'Futebol'
 )
